6055aba6b345b49f4f2fe3960e137c2c8d99508e
Warhair <36608769+Warhair@users.noreply.github.com>
2023-03-02 13:19:00 +0100

    feladat1

---
+++ SzamolosFeladatok/src/ciklusok/Szamok.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ciklusok;
+
+/**
+ *
+ * @author tegze
+ */
+public class Szamok {
+    
+public static void main(String[] args) {
+    for (int k = 2; k < 5; k++){
+        for (int j = 2; j < 5; j++){
+        for (int i = 2; i <= 4; i++) {
+            System.out.println(k+""+j+""+i);
+    }
+        }
+    }
+    
+    
+ 
+    }
+}
