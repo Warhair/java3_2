fe12d01a13fa4d39bbb43cf43426c8959563ef9d
Warhair <36608769+Warhair@users.noreply.github.com>
2023-03-02 13:42:53 +0100

    ismetlodikekesz

--- SzamolosFeladatok/src/ciklusok/Szamok.java
+++ SzamolosFeladatok/src/ciklusok/Szamok.java
@@ -9,17 +9,33 @@ package ciklusok;
  * @author tegze
  */
 public class Szamok {
-    
-public static void main(String[] args) {
-    for (int k = 2; k < 5; k++){
-        for (int j = 2; j < 5; j++){
-        for (int i = 2; i <= 4; i++) {
-            System.out.println(k+""+j+""+i);
-    }
+
+    public static void main(String[] args) {
+        String szoveg = "ismetlodessel";
+        int db = 0;
+        if (szoveg == "ismetlodessel") {
+            for (int k = 2; k < 5; k++) {
+                for (int j = 2; j < 5; j++) {
+                    for (int i = 2; i <= 4; i++) {
+                        System.out.println(k + "" + j + "" + i);
+                        db++;
+                    }
+                }
+            }
+        } else {
+            for (int k = 2; k < 5; k++) {
+                for (int j = 2; j < 5; j++) {
+                    for (int i = 2; i <= 4; i++) {
+                        if (j == k || j == i || k == i) {
+
+                        } else {
+                            System.out.println(k + "" + j + "" + i);
+                            db++;
+                        }
+                    }
+                }
+            }
         }
-    }
-    
-    
- 
+        System.out.println(db);
     }
 }
